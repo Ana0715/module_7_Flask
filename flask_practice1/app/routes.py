7c0aca98a6808013395abfa327ee43a53fb67f07
MSI <orlovaana0715@gmail.com>
2025-11-01 13:29:01 +0200

    Flask. Урок 9

--- flask_practice1/app/routes.py
+++ flask_practice1/app/routes.py
@@ -1,17 +1,32 @@
 from flask import render_template, request, redirect, jsonify, url_for
 from app import app
+from datetime import datetime
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    current_time = datetime.now()
+    return render_template('index.html', current_time=current_time)
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    team_members = [
+        {'name': 'Alice', 'role': 'Developer'},
+        {'name': 'Bob', 'role': 'Designer'},
+        {'name': 'Charlie', 'role': 'Project Manager'}
+    ]
+    return render_template('about.html', team_members=team_members)
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')
+    customer_care = {
+        'number': '3-45-89',
+        'address': {
+            'city': 'London',
+            'street':'Carey Street',
+            'building': 666
+        }
+    }
+    return render_template('contact.html', customer_care=customer_care)
 
 @app.route('/submit', methods=['POST', 'GET'])
 def submit():
